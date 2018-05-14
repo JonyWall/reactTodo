c44484ffa0ccae9ed2fbc3546a3ebeebf786c6a8
DESKTOP-CEPHRE3\Jonathan <jonyw.tkd@gmail.com>
2018-05-14 13:10:09 +0300

    push test

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">React - Todos</h1>
+            <h1 className="App-title">React - Todos app</h1>
           </header>
           <Tabs
             value={this.state.value}
